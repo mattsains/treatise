2a6728e102bd66a3a2d92183242979417172679d
Matthew Sainsbury <matthew@sainsbury.za.net>
2015-03-22 17:31:15 +0200

    Added the first Euler problem to tests

---
+++ New Virtual Machine/tests/mul35.asm
@@ -0,0 +1,24 @@
+movc r1, 1
+movc r5, 0
+
+loop:
+    mov r2, r1
+    divc r1, 3
+    mov r1, r2
+    jcmpc r0, 0, acc, acc, cont
+    cont:
+    divc r1, 5
+    mov r1, r2
+    jcmpc r0, 0, acc, acc, reject
+    acc:
+    add r5, r1
+    reject:
+    addc r1, 1
+
+    jcmpc r1, below, loop, end, end
+
+end:
+    in r1
+
+below:
+dq 1000
