545f91b00d72e11fda5f9c0ab3f339fc6d1ecc1d
Matthew Sainsbury <matthew@sainsbury.za.net>
2015-04-18 15:32:28 +0200

    Updated table VM to new entry

--- Virtual Machines/Table Only/vm.asm
+++ Virtual Machines/Table Only/vm.asm
@@ -29,8 +29,34 @@ vm_start:
         mov rdi, program_loaded
         call print
         pop rsi
+        
+        ;Create the first stack frame
+        lodsq ;number of locals
+        ;advance rsi while we have number of locals
+        mov rbx, rax
+        add rbx, 63
+        shr rbx, 6
+        shl rbx, 3 ;(locals.ceil(64)/64)*8
+        add rsi, rbx
 
-        dispatch:
+        mov rdi, rax
+        add rdi, 11 ;+sham registers saving+td ptr
+        shl rdi, 3 ;rdi*8
+        push rax
+        push rsi
+        call malloc
+        mov rbp, rax
+        pop rsi
+        pop rax
+        add rbp, 0x48 ;skip over sham registers
+        mov qword [rbp], 0 ;previous rbp field is null
+        add rbp, 8
+        mov rax, rbp ;use rax for flags+ptr
+        or rax, 2 ;obj flag
+        mov [rbp], rax
+        ;assuming no parameters
+        ;fall through to dispatch
+
         xor rax, rax ;the high portion of rax needs to stay cleared forever
         ;Dispatch now
         %include "templates/dispatch"
