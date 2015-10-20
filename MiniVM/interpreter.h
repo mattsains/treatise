e3e3f43f64c3054510e77869de8e7e1e3f95ee06
Matthew Sainsbury <matthew@sainsbury.za.net>
2015-10-20 11:10:59 +0200

    Deleted old experiments

--- MiniVM/interpreter.h
+++
@@ -1,8 +0,0 @@
-uint16_t register_or_constant_value (uint16_t operand);
-
-void execute(struct lua_function function, struct lua_type* arguments, uint32_t argument_count);
-
-void enforce_type(struct lua_type var, char type);
-
-uint16_t register_or_constant_value (uint16_t operand);
-
--- MiniVM/main.h
+++
@@ -1,9 +0,0 @@
-int main(int argc, char* argv[]);
-
-struct lua_function parse_function(FILE* file);
-
-struct lua_string parse_string(FILE* file);
-
-uint32_t endian_convert32(uint32_t in);
-
-uint64_t endian_convert64(uint64_t in);
